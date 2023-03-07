ee8f6103edde82f726258df7e612f3ce30a23f2b
Guillaume Dieudonné <8122069+GuiDieudo@users.noreply.github.com>
2023-03-07 11:35:36 +0100

    feat: updating Fantom Tesnet RPC URL for more stability

--- packages/dapp/packages/wagmi/chains/chains.ts
+++ packages/dapp/packages/wagmi/chains/chains.ts
@@ -94,6 +94,12 @@ export const fantomTestnet: Chain = {
   network: 'fantom-testnet',
   nativeCurrency: { name: 'Fantom', symbol: 'FTM', decimals: 18 },
   rpcUrls: {
+    /* 
+      https://endpoints.omniatech.io/v1/fantom/testnet/public	
+      https://rpc.ankr.com/fantom_testnet	
+      https://rpc.testnet.fantom.network	
+      https://fantom-testnet.public.blastapi.io
+    */
     // default: 'https://rpc.testnet.fantom.network',
     default: 'https://fantom-testnet.public.blastapi.io	',
   },
